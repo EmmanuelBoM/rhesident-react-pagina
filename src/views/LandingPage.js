33099ec8260878d015357f2669f958897d4a5fa7
EmmanuelBoM <a01276310@itesm.mx>
2022-02-06 23:33:39 -0600

    Arregla estilos de Landing Page

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,9 +1,8 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import '../styles/base.css'
 import '../styles/LandingPage.css'
 import NavHeader from '../components/NavHeader'
 import Footer from '../components/Footer'
-import img from '../assets/img.jpg'
 import iconoHuella from '../assets/huella1.svg'
 import iconoCausa from '../assets/icono_causa.svg'
 import iconoMas from '../assets/icono_mas.svg'
@@ -12,11 +11,17 @@ import "animate.css/animate.min.css";
 import { AnimationOnScroll } from 'react-animation-on-scroll';
 import {Link} from 'react-router-dom'
 import CustomMailchimpForm from '../components/CustomMailchimpForm'
+import iconoIntenciones from '../assets/icono_intenciones.png'
+
+// Firebase Imports
+import {db} from '../firebaseConfig'
+import {collection,getDoc, doc} from "@firebase/firestore";
 
 function LandingPage() {
 
     const [isVideoVisible, setIsVideoVisible] = useState(false);
-   
+    const [portadaImg, setPortadaImg] = useState('')
+
     function autoPlayVideo(){
       if (window.scrollY>=500){
         setIsVideoVisible(true)
@@ -26,6 +31,16 @@ function LandingPage() {
       }
     }
 
+    const portadaRef = doc(db, "recursosGenerales", "5ePochyXKfxpLa6MK32Q")
+    useEffect (()=>{
+        const getPortada = async () => {
+          const portadaDoc = await getDoc(portadaRef);
+          setPortadaImg(portadaDoc.data().url)
+        }
+    
+        getPortada();
+      }, []);
+
 
   window.addEventListener('scroll', autoPlayVideo)
     
@@ -33,34 +48,37 @@ function LandingPage() {
         <main>
             <NavHeader textColor='blanco'></NavHeader>
             <section className="hero-landing">
-                <svg className="main-svg" width="1925" height="1422" viewBox="0 0 1925 1422" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <defs>
-                        <pattern id='img1' patternUnits='userSpaceOnUse' x="0" y="0" width="1925" height="1422">
-                            <image href={img} width="2000" height="1500" />             
-                        </pattern>
-                    </defs>
-                    <path opacity="1" d="M1917.74 278.382L1915.92 269.839C1880.87 101.402 1723.16 -14.0913 1550.69 1.73517C1515.37 4.95632 1480.76 9.03925 1448.92 14.1142C1284.29 40.4939 -245.865 72.4527 34.5075 445.915C314.88 819.377 837.031 1433.22 945.024 1420.84C1053.02 1408.46 1264.65 1347.64 1313.41 1211.56C1332.55 1158.14 1342.02 1072.37 1346.46 987.841C1354.53 834.396 1467.52 707.071 1619.78 678.919L1650.75 673.196C1835.62 638.892 1955.67 461.405 1917.74 278.382Z" fill="url(#img1)"/>
-                    <path opacity="0.7" d="M1917.74 278.382L1915.92 269.839C1880.87 101.402 1723.16 -14.0913 1550.69 1.73517C1515.37 4.95632 1480.76 9.03925 1448.92 14.1142C1284.29 40.4939 -245.865 72.4527 34.5075 445.915C314.88 819.377 837.031 1433.22 945.024 1420.84C1053.02 1408.46 1264.65 1347.64 1313.41 1211.56C1332.55 1158.14 1342.02 1072.37 1346.46 987.841C1354.53 834.396 1467.52 707.071 1619.78 678.919L1650.75 673.196C1835.62 638.892 1955.67 461.405 1917.74 278.382Z" fill="#164453"/>
+                <svg className="main-svg"  viewBox="0 0 1920 850" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <defs>
+                    <pattern id='img1' patternUnits='userSpaceOnUse' x="0" y="0" width="1925" height="1422">
+                        <image href={portadaImg} className='img-portada-landing' />             
+                    </pattern>
+                  </defs>
+                  <path opacity="1" d="M1920 583.5C1633.68 1100.34 711.057 681.759 174.892 802.451C105.911 817.977 48.0377 828.408 -3.36576e-06 816L2.3076e-05 0.500065L1918.46 0.500131L1920 583.5Z" fill="url(#img1)"/>
+                  <path  opacity="0.7" d="M1920 583.5C1633.68 1100.34 711.057 681.759 174.892 802.451C105.911 817.977 48.0377 828.408 -3.36576e-06 816L2.3076e-05 0.500065L1918.46 0.500131L1920 583.5Z" fill="#0C3E4D"/>
                 </svg>
+                
                 <h1 className="titulo-landing blanco">
-                    Rhesident <span className="amarillo">Org.</span>
+                    Rhesident Org.
                 </h1>
-                <h3  className="subtitulo-landing blanco">
-                    Somos una organización de Regeneración Comunitaria Multidisciplinaria.
+                <h3  className="subtitulo-landing amarillo">
+                    Somos una organización de <br/> <span className="bold">Regeneración Comunitaria Multidisciplinaria.</span> 
                 </h3>
                 <p className="descripcion-hero blanco">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-                    animi hic tempore officia deserunt nulla. At perferendis, sunt
-                    maiores amet aliquam eveniet architecto nostrum nobis fugit cum
-                    consequatur officiis voluptate eaque doloremque! 
+                Somos una organización <span className="bold">teal</span>  de regeneración comunitaria multidisciplinaria, basada en un sistema holístico e integral;  mediante acciones locales, pretendemos transformar la interacción comunitaria, incentivando el bienestar individual y colectivo.
                 </p>
+                <div className="scrolldown-cont-small">
+                    <i class="fa-solid fa-angles-down scroll-down-link" ></i>
+                </div>
+
                 <div className="hero-ctas">
                     <AnimationOnScroll animateIn="animate__fadeInLeft" animateOut='animate__fadeOutLeft' offset={190}>
-                      <div className="cta">
-                          <img src={iconoHuella} alt="" className="icono-cta" />
+                        <div className="cta">
+                            <div className="icono-cta">
+                                <img src={iconoHuella} alt="" className="img-cta" />
+                            </div>
                           <p className='texto-cta negro'>
-                              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-                              maxime, facilis iste delectus autem alias.
+                          Únete al equipo y otorga tus talentos a este movimiento.
                           </p>
                           <Link to='/voluntariado'>
                             <button className="btn-cta">Conviértete en voluntario</button>
@@ -70,22 +88,24 @@ function LandingPage() {
                     </AnimationOnScroll>
                     <AnimationOnScroll animateIn="animate__fadeInRight" animateOut='animate__fadeOutRight' offset={190}>
                       <div className="cta">
-                          <img src={iconoCausa} alt="" className="icono-cta" />
+                          <div className="icono-cta">
+                            <img src={iconoCausa} alt="" className="img-cta" />
+                          </div>
+                         
                           <p className='texto-cta megro'>
-                              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-                              maxime, facilis iste delectus autem alias.
+                          Con tu ayuda, podemos lograr grandes transformaciones.
                           </p>
                           <Link to='/como-apoyar'>
-                             <button className="btn-cta">Apoya nuestra causa </button>
+                              
+                             <button className="btn-cta">Apoya<br /> nuestra causa </button>
                           </Link>
-                         
                       </div>
                     </AnimationOnScroll>
                 </div>
         </section>
         
         <div className="scrolldown-cont">
-          <img src={downArrow} alt="" className="scroll-down-link scroll-down-arrow" />
+            <i class="fa-solid fa-angle-down scroll-down-link-green" ></i>
         </div>
 
         <section className="video-principal">
@@ -133,14 +153,24 @@ function LandingPage() {
         </div>
         
         <section className="conoce">
-          <h2 className="negro">¡Conócenos!</h2>
-          <p className='descripcion-seccion'><span className="bold">Desliza hacia abajo</span>  para conocer<br /> los aspectos  que definen a <span className="bold">Rhesident Org.</span> </p>
+            <h2 className="negro">¡Conócenos!</h2>
+            <p className='descripcion-seccion'><span className="bold">Desliza hacia abajo</span>  para conocer<br /> los aspectos  que definen a <span className="bold">Rhesident Org.</span> </p>
           
-          <AnimationOnScroll animateIn="animate__fadeIn"  offset={100} >
-            <div className="elemento-conoce"></div>
-          </AnimationOnScroll>
-          <div className="nodes-cont">
-          </div>
+            <AnimationOnScroll animateIn="animate__fadeIn"  offset={100} >
+                <div className="item-conoce">
+                    <svg className='huella-conoce' width="471" height="426" viewBox="0 0 471 426" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path className='path-conoce' d="M40.5948 34.4589L40.5961 34.458C87.9233 0.385331 149.926 -4.30329 202.253 21.2961C232.481 36.0871 267.288 51.946 290.191 58.5908C313.062 65.2314 332.898 79.4079 353.747 96.7009C359.129 101.165 364.588 105.846 370.177 110.639C386.022 124.228 402.912 138.713 422.06 151.682C434.42 160.054 443.273 168.906 449.666 179.206C456.068 189.521 460.165 201.552 462.652 216.508C467.294 244.419 466.224 281.559 464.679 335.213C464.552 339.628 464.421 344.156 464.291 348.799C463.868 363.835 462.655 375.409 460.635 384.291C458.613 393.18 455.849 399.071 452.535 403.021C446.191 410.585 436.516 412.52 419.997 411.659C413.54 411.322 406.268 410.57 398.15 409.73C365.305 406.332 318.625 401.503 256.204 417.006C219.151 426.208 191.482 418.865 169.269 401.631C146.744 384.154 129.427 356.192 114.134 323.389C103.982 301.613 94.9114 278.156 85.8043 254.605C81.1717 242.625 76.5296 230.621 71.731 218.801C57.6226 184.049 42.216 151.064 22.0185 126.972C5.27119 106.995 2.43836 89.8345 6.87797 75.1449C11.4465 60.0286 24.0447 46.366 40.5948 34.4589Z" stroke="#F0E967" stroke-width="10"/> 
+                    </svg>
+                    <h3 className="verde subt-conoce">Nuestras <br /> intenciones</h3>
+                    <p className="negro text-conoce">Aquí encontrarás todo lo que motiva cada una de nuestras actividades y colaboraciones.</p>
+                    <div className="corazon-conoce">
+                        <img src={iconoIntenciones} alt="" />
+                    </div>
+                </div>
+                
+            </AnimationOnScroll>
+            <div className="nodes-cont">
+            </div>
         </section>
 
         <section className="round-ctas">
